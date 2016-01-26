2762c418790025a42cfce7c12a2f5b3805e8e405
mike@kaslingaircraft.com <mike@kaslingaircraft.com>
2016-01-26 09:38:54 -0600

    Add acc class

--- src/Sensor_Logger/Sensor_Logger.py
+++ src/Sensor_Logger/Sensor_Logger.py
@@ -43,9 +43,73 @@ class L3G4200DDriver:
         gyroIntList = struct.unpack('hhh',gyroByteList[0:6])
         return gyroIntList;
     
+
+class LSM303DLHAccDriver:
+    #ACC defines
+    ACC_ADDRESS       = 0x18#0001 1000
+    CTRL_REG1_A       = 0x20
+    CTRL_REG2_A       = 0x21
+    CTRL_REG3_A       = 0x22
+    CTRL_REG4_A       = 0x23
+    CTRL_REG5_A       = 0x24
+    CTRL_REG6_A       = 0x25
+    HP_FILTER_RESET_A = 0x25
+    REFERENCE_A       = 0x26
+    STATUS_REG_A      = 0x27
     
+    OUT_X_L_A         = 0x28
+    OUT_X_H_A         = 0x29
+    OUT_Y_L_A         = 0x2A
+    OUT_Y_H_A         = 0x2B
+    OUT_Z_L_A         = 0x2C
+    OUT_Z_H_A         = 0x2D
+    
+    READ = 0x80
+    
+    def __init__(self):
+        self.acc = FT232H.I2CDevice(ft232h,self.ACC_ADDRESS,400000) 
+    
+    def Setup(self):
+        self.acc.write8(self.CTRL_REG1_A, 0x3F)
+        self.acc.write8(self.CTRL_REG2_A, 0x00)
+        self.acc.write8(self.CTRL_REG3_A, 0x00)
+        self.acc.write8(self.CTRL_REG4_A, 0x30)
+        self.acc.write8(self.CTRL_REG5_A, 0x00)
+        
+    def Read(self):
+        accByteList = self.acc.readList(self.OUT_X_L_A | self.READ, 6)
+        accIntList = struct.unpack('hhh',accByteList[0:6])
+        return accIntList    
+        
+#end classes ---------------------------------------------------------        
 gyro = L3G4200DDriver()
+acc = LSM303DLHAccDriver()
 
 gyro.Setup()
+acc.Setup()
 
 print gyro.Read()
+print acc.Read()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
