8adee269a4adf206943d972568fb42c3c8b4710a
mike@kaslingaircraft.com <mike@kaslingaircraft.com>
2016-01-26 10:06:21 -0600

    Add mag code

--- src/Sensor_Logger/Sensor_Logger.py
+++ src/Sensor_Logger/Sensor_Logger.py
@@ -40,10 +40,45 @@ class L3G4200DDriver:
 
     def Read(self):
         gyroByteList = self.gyro.readList(self.L3G_OUT_X_L | self.READ, 6)
-        gyroIntList = struct.unpack('hhh',gyroByteList[0:6])
+        gyroIntTouple = struct.unpack('hhh',gyroByteList[0:6])
+        gyroIntList = list(gyroIntTouple)
         return gyroIntList;
     
-
+class LSM303DLHMagDriver:
+    #mag defines ST HMC5983DLHC - will work with the HMC5883L
+    MAG_ADDRESS     = 0x1E  #0001 1110
+    HMC5983_CRA_REG = 0x00
+    HMC5983_CRB_REG = 0x01
+    HMC5983_MR_REG  = 0x02
+    HMC5983_OUT_X_H = 0x03
+    HMC5983_OUT_X_L = 0x04
+    HMC5983_OUT_Z_H = 0x05
+    HMC5983_OUT_Z_L = 0x06
+    HMC5983_OUT_Y_H = 0x07
+    HMC5983_OUT_Y_L = 0x08
+    
+    HMC5983_ID_A     = 0x0A
+    HMC5983_ID_B     = 0x0B
+    HMC5983_ID_C     = 0x0C
+    HMC5983_WHO_AM_I = 0x0F
+    READ = 0x80
+    def __init__(self):
+        self.mag  = FT232H.I2CDevice(ft232h,self.MAG_ADDRESS,400000)
+    def Setup(self):
+        self.mag.write8(self.HMC5983_CRA_REG, 0x9C)
+        self.mag.write8(self.HMC5983_CRB_REG, 0x60)
+        self.mag.write8(self.HMC5983_MR_REG, 0x80)
+    def Read(self):
+        magByteList = self.mag.readList(self.HMC5983_OUT_X_H | self.READ,6)
+        magIntTouple = struct.unpack('>hhh',magByteList[0:6])
+        magIntList = list(magIntTouple)
+        magIntList[1], magIntList [2] = magIntList[2], magIntList[1]
+#         Z = magIntList[1]
+#         Y = magIntList[2]
+#         magIntList[1] = Y
+#         magIntList[2] = Z
+        return magIntList    
+    
 class LSM303DLHAccDriver:
     #ACC defines
     ACC_ADDRESS       = 0x18#0001 1000
@@ -78,19 +113,22 @@ class LSM303DLHAccDriver:
         
     def Read(self):
         accByteList = self.acc.readList(self.OUT_X_L_A | self.READ, 6)
-        accIntList = struct.unpack('hhh',accByteList[0:6])
+        accIntTouple = struct.unpack('hhh',accByteList[0:6])
+        accIntList = list(accIntTouple)
         return accIntList    
         
 #end classes ---------------------------------------------------------        
 gyro = L3G4200DDriver()
 acc = LSM303DLHAccDriver()
+mag = LSM303DLHMagDriver()
 
+mag.Setup()
 gyro.Setup()
 acc.Setup()
 
 print gyro.Read()
 print acc.Read()
-
+print mag.Read()
 
 
 
